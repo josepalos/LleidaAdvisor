8790f4901ceb098c0061b2b02a94d0f3a554c086
Daniel Galan <danigv19@gmail.com>
2019-11-11 17:56:04 +0100

    Added cuisine_details and price. Don't work

--- src/scrapper.py
+++ src/scrapper.py
@@ -3,7 +3,7 @@ import typing
 import utils
 import sys
 
-BASE_URL = "https://www.tripadvisor.es"
+BASE_URL = "https://www.tripadvisor.com"
 RESTAURANT_PAGINATION_URL = BASE_URL + "/RestaurantSearch?" \
                                        "&geo={geo}" \
                                        "&sortOrder=relevance" \
@@ -79,36 +79,76 @@ def fetch_restaurant_info(name: str, restaurant_url: str) -> Restaurant:
     phone = utils.get_text_elem(bs, "div", "data-blcontact", "PHONE").text
 
     # Canviar a float
-    score = utils.get_text_elem(bs, "span", "class", "restaurants-detail-overview-cards-RatingsOverviewCard__overallRating--nohTl").text
+    score = float(utils.get_text_elem(bs, "span", "class", "restaurants-detail-overview-cards-RatingsOverviewCard__overallRating--nohTl").text.replace(',', '.'))
 
     all_scores = utils.get_text_all_elems(bs, "div", "class", "restaurants-detail-overview-cards-RatingsOverviewCard__ratingQuestionRow--5nPGK")
+    all_scores_len = len(all_scores)
 
-    if(len(all_scores)==0):
+    if all_scores_len == 0:
         score_food = None
         score_service = None
         score_price = None
+        score_ambient = None
+
     else:
         score_food = int(utils.get_bubble_score(str(utils.get_text_elem(all_scores[0], "span", "class", "ui_bubble_rating"))))/10
         score_service = int(utils.get_bubble_score(str(utils.get_text_elem(all_scores[1], "span", "class", "ui_bubble_rating"))))/10
         score_price = int(utils.get_bubble_score(str(utils.get_text_elem(all_scores[2], "span", "class", "ui_bubble_rating"))))/10
 
-    restaurant_details = utils.get_text_all_elems(bs, "div", "class", "restaurants-detail-overview-cards-DetailsSectionOverviewCard__tagText--1OH6h");
-    print(len(restaurant_details))
+        if all_scores_len == 4:
+            score_ambient = int(utils.get_bubble_score(str(utils.get_text_elem(all_scores[3], "span", "class", "ui_bubble_rating"))))/10
+        else:
+            score_ambient = None
+
+    restaurant_details_top = utils.get_text_all_elems(bs, "div", "class", "restaurants-detail-overview-cards-DetailsSectionOverviewCard__detailsSummary--evhlS")
+    restaurant_details_bottom = utils.get_text_all_elems(bs, "div", "data-tab", "TABS_DETAILS")
+
+    if len(restaurant_details_top) == 0 and len(restaurant_details_bottom) > 0:
+        print("bottom")
+
+    elif len(restaurant_details_bottom) == 0 and len(restaurant_details_top) > 0:
+
+        titles = utils.get_text_all_elems(restaurant_details_top[0], "div", "class", "restaurants-detail-overview-cards-DetailsSectionOverviewCard__categoryTitle--2RJP_")
+        descriptions = utils.get_text_all_elems(restaurant_details_top[0], "div", "class", "restaurants-detail-overview-cards-DetailsSectionOverviewCard__tagText--1OH6h")
+
+        cuisine_details = ""
+        price = None
+
+        for title in titles:
+            for description in descriptions:
+                if title.text == "PRICE RANGE":
+                    price = description.text
+                else:
+                    cuisine_details += title.text + ":" + description.text + ", "
+
+        print(cuisine_details)
+        #print(restaurant_details_top)
+
+        #for element in restaurant_details_top:
+         #   print(utils.get_text_elem(element, "div", "class", "restaurants-detail-overview-cards-DetailsSectionOverviewCard__categoryTitle--2RJP_").text)
+          #  print(utils.get_text_elem(element, "div", "class", "restaurants-detail-overview-cards-DetailsSectionOverviewCard__tagText--1OH6h").text)
+
+        #print(len(restaurant_details_top))
+
+    else:
+        prices = None
+        cuisine_details = None
+
+
 
     # prices = utils.get_text_elem(bs, "div", "class", "restaurants-detail-overview-cards-DetailsSectionOverviewCard__tagText--1OH6h").text
-
-    # score_ambient: float,
+    # restaurants-detail-overview-cards-DetailsSectionOverviewCard__detailCard--WpImp
     # opening_hours = utils.get_text_elem(bs, "div", "class", "public-location-hours-LocationHours__hoursPopover--2h1HP")
     # cuisine_details = utils.get_text_elem(bs, "div", "class", "restaurants-detail-overview-cards-DetailsSectionOverviewCard__tagText--1OH6h")
 
     excellency_certificate = False
 
-    if(utils.get_text_elem(bs, "div", "class", "restaurants-detail-overview-cards-RatingsOverviewCard__award--31yzt")):
+    if utils.get_text_elem(bs, "div", "class", "restaurants-detail-overview-cards-RatingsOverviewCard__award--31yzt"):
         excellency_certificate = True
 
 
 
-    print(name + " " + direction + " " + phone + " " + " Score: " + score + " Score food: " + str(score_food) + " Score service: " + str(score_service) + " Score price: " + str(score_price) + " " + str(excellency_certificate))
+    #print(name + " " + direction + " " + phone + " " + " Score: " + str(score) + " Score food: " + str(score_food) + " Score service: " + str(score_service) + " Score price: " + str(score_price) + " " + str(excellency_certificate))
 
     return None
 
