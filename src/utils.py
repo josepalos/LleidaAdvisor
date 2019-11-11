30a97149e5c0695995db532b825b81421e72dbe0
Josep <josep.alos.pascual@hotmail.com>
2019-11-11 20:24:50 +0100

    Add retries when the requests fail.

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,52 @@
+import functools
+import sys
+import time
+
 import requests
 import bs4
 from bs4 import Tag, NavigableString
 
 from scrapper import generate_reviews_url
 
+MAX_DELAY = 10
 
+
+def retry_if_fail(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        delay = 0
+        while True:
+            time.sleep(delay)
+            try:
+                return f(*args, **kwargs)
+            except requests.exceptions.RequestException:
+                print("Error on request, applying delay", file=sys.stderr)
+                delay = min(delay + 1, MAX_DELAY)
+                continue
+    return wrapper
+
+
+@retry_if_fail
 def get_bs(url, *args, **kwargs):
     response = requests.get(url)
     response.raise_for_status()
     return bs4.BeautifulSoup(response.text, 'html.parser')
 
 
+@retry_if_fail
 def post_bs(url, *args, **kwargs):
     response = requests.post(url, *args, **kwargs)
     response.raise_for_status()
     return bs4.BeautifulSoup(response.text, "html.parser")
 
 
-def get_text_elem(elem, tag, a , argument):
-    return elem.find(tag, attrs={a:argument})
+def get_text_elem(elem, tag, a, argument):
+    return elem.find(tag, attrs={a: argument})
 
-def get_text_all_elems(elem, tag, a , argument):
+
+def get_text_all_elems(elem, tag, a, argument):
     return elem.findAll(tag, attrs={a: argument})
+
 
 def get_bubble_score(str):
     return str[37:-9]
